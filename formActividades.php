97a32c2e96e72aa5aea9f63894824354849e66c2
Hp Pavilion <info@bitinformatica.com.py>
2023-04-19 17:10:09 -0400

    Ultimo commit de 19/04/2023 No hubieron cambios

--- formActividades.php
+++ formActividades.php
@@ -17,7 +17,7 @@ require 'Views/sideBar.php';
             <div class="page-breadcrumb bg-white">
                 <div class="row align-items-center">
                     <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
-                        <h4 class="page-title">Actividades</h4>
+                        <h4 class="page-title">Formulario de Actividades</h4>
                     </div>
                     <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
                         <div class="d-md-flex">
